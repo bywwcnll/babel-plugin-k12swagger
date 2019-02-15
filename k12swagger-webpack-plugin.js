d154963d314df4fb0c26bc001032770703d7f2d7
lswang <lswang@wysource.com.cn>
2019-02-15 10:58:18 +0800

    增加webpack插件

---
+++ k12swagger-webpack-plugin.js
@@ -0,0 +1,32 @@
+const fs = require('fs')
+const path = require('path')
+
+const tmpDirPath = path.resolve('node_modules/.swaggerTmp')
+const tmpDepsFilePath = path.resolve(tmpDirPath, '__tmpSwaggerDeps.json')
+module.exports = class K12swaggerWebpackPlugin {
+  apply (compiler) {
+    compiler.hooks.entryOption.tap('k12swagger-webpack-plugin', (params) => {
+      if (!fs.existsSync(tmpDirPath)) fs.mkdirSync(tmpDirPath)
+      if (process.env.MIN_VUEX === 'search') {
+        if (fs.existsSync(tmpDepsFilePath)) fs.unlinkSync(tmpDepsFilePath)
+        fs.writeFileSync(tmpDepsFilePath, JSON.stringify({
+          result: {},
+          date: new Date(),
+          MIN_VUEX: process.env.MIN_VUEX
+        }))
+      } else if (process.env.MIN_VUEX === 'filter' && fs.existsSync(tmpDepsFilePath)) {
+        let fileContents = fs.readFileSync(tmpDepsFilePath, { encoding: 'utf8' })
+        let fileContentsData = JSON.parse(fileContents || '{"result": {}}')
+        fs.writeFileSync(tmpDepsFilePath, JSON.stringify({
+          ...fileContentsData,
+          MIN_VUEX: process.env.MIN_VUEX
+        }))
+      } else {
+        fs.writeFileSync(tmpDepsFilePath, JSON.stringify({
+          result: {},
+          date: new Date()
+        }))
+      }
+    })
+  }
+}
